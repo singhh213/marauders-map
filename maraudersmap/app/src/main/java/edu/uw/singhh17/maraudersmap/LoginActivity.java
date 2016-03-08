afe0139f909aabb207c6c8ca87cd8d7403c3227e
jaimelenn <jaimelenn.uemura@gmail.com>
2016-03-07 22:17:38 -0800

    recognizes first time openning app

--- maraudersmap/app/src/main/java/edu/uw/singhh17/maraudersmap/LoginActivity.java
+++ maraudersmap/app/src/main/java/edu/uw/singhh17/maraudersmap/LoginActivity.java
@@ -1,6 +1,7 @@
 package edu.uw.singhh17.maraudersmap;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,46 +17,69 @@ public class LoginActivity extends AppCompatActivity {
     private Button signIn;
     Firebase ref;
 
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
 
-        name = (EditText)findViewById(R.id.name);
-        signIn = (Button) findViewById(R.id.signIn);
-
-        Firebase.setAndroidContext(this);
-        ref = new Firebase("https://map-users.firebaseio.com/users" ).child("UserDetail");
+        prefs = getSharedPreferences("Map", MODE_PRIVATE);
 
 
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Creating firebase object
-
-                //Getting values to store
-                String names = name.getText().toString().trim();
-
-                //Creating Person object
-                UserDetail user = new UserDetail();
-
-                //Adding values
-                user.setFullName(names);
-
-                //Storing values to firebase
-
-                ref.push().setValue(user);
-
-                Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
-                startActivity(intent);
-            }
-
-
-        });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (prefs.getBoolean("firstrun", true)) {
+
+
+            setContentView(R.layout.activity_login);
+
+
+
+            name = (EditText)findViewById(R.id.name);
+            signIn = (Button) findViewById(R.id.signIn);
+
+            Firebase.setAndroidContext(this);
+            ref = new Firebase("https://map-users.firebaseio.com/users" ).child("UserDetail");
+
+
+            signIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Creating firebase object
+
+                    //Getting values to store
+                    String names = name.getText().toString().trim();
+
+                    //Creating Person object
+                    UserDetail user = new UserDetail();
+
+                    //Adding values
+                    user.setFullName(names);
+
+                    //Storing values to firebase
+
+                    ref.push().setValue(user);
+
+                    Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
+                    startActivity(intent);
+                }
+
+
+            });
+
+            // Do first run stuff here then set 'firstrun' as false
+            // using the following line to edit/commit prefs
+            prefs.edit().putBoolean("firstrun", false).commit();
+        } else {
+            Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
+            startActivity(intent);
+        }
+    }
 
 
 
